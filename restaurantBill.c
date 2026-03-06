6f080bd05a819fd97aa17a5c68702f657c95213a
Evan Plascencia-Rambish <evanp@cpp.edu>
2026-03-06 15:31:15 -0800

    Created folder and outline for Restaurant Bill

---
+++ restaurantBill.c
@@ -0,0 +1,7 @@
+// restaurantBill.c
+// Program to calculate tax and tip for a random meal
+// 1. Define meal options and prices
+// 2. Randomly select a meal
+// 3. Read tax and tip from command line
+// 4. Calculate tax amount, tip amount, and total
+// 5. Print meal, tax, tip (to STDERR), total 
