020cb85a975a017561071f9a320226bc04e5ffcf
Evan Plascencia-Rambish <evanp@cpp.edu>
2026-03-06 15:50:36 -0800

    Added meal options and random selection of food

--- restaurantBill.c
+++ restaurantBill.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 // restaurantBill.c
 // Program to calculate tax and tip for a random meal
 // 1. Define meal options and prices
@@ -5,3 +9,21 @@
 // 3. Read tax and tip from command line
 // 4. Calculate tax amount, tip amount, and total
 // 5. Print meal, tax, tip (to STDERR), total 
+
+int main(int argc, char *argv[]) {
+
+    // Define meal options and prices
+
+    const char *meals[] = {"Salad", "Soup", "Sandwich", "Pizza"};
+    double prices[] = {9.95, 4.55, 13.25, 22.35};
+
+    // Randomly select a meal
+
+    srand(time(NULL));
+    int mealIndex = rand() % 4;
+    double mealCost = prices[mealIndex];
+
+    printf("Randomly selected meal: %s - $%.2f\n", meals[mealIndex], mealCost);
+
+    return 0;
+}
